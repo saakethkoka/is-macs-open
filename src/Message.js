b9493b7e16a4cc582dab85e1ca9ce21b9c66de52
saakethkoka <63078934+saakethkoka@users.noreply.github.com>
2022-02-08 15:50:05 -0600

    MVP

---
+++ src/Message.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+
+class Message extends Component {
+
+  state = {
+
+  }
+
+  async httpGet(theUrl)
+  {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+  }
+
+
+  componentDidMount() {
+    this.httpGet("https://xtzrptp2si.execute-api.us-east-2.amazonaws.com/test/ismacsresource").then(response => {
+      this.setState({
+        isOpen: response
+      })
+    })
+  }
+
+  setText(){
+    if(this.state.isOpen){
+      return "yes"
+    }
+    else{
+      return "no"
+    }
+  }
+
+  render() {
+    return (
+      <h1>
+        {this.setText()}
+      </h1>
+    );
+  }
+}
+
+export default Message;
